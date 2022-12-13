599263c4cc60c0afea9b29a5bbf2658e5a2ad7c3
Diego_Mora <aguilarluisdi@estudiantec.cr>
2022-12-13 00:47:19 -0600

    Prints for input + validations

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,20 +1,39 @@
 from logic.Simulator import Simulator
 
-
 def printAuthors():
+    print("_____________________________________________________________\n")
     print("Laboratorio 1 - Electiva de Simulacion")
     print("Integrantes:")
     print("Rose Yesenia Campos Carvajal - 2020072817")
     print("Luis Diego Mora Aguilar - 2018147110")
+    print("_____________________________________________________________\n\n")
 
 def askValues():
     resultado = []
-    print("C = Caras: ")
-    resultado.append(int(input()))
-    print("N = Numero de veces que se lanza el dado: ")
-    resultado.append(int(input()))
-    print("K = Experimentos: ")
-    resultado.append(int(input()))
+    invalidOption = True
+    #Map to make the menu choosing easier, maping each option number with the sides of the dice
+    optionsMap = {1:4, 2:6, 3:8, 4:12, 5:20}
+    print("Por favor, ingrese los datos para la simulacion.\n")
+    while invalidOption:
+        try:
+            print("C = Caras, elija la cantidad de caras que desea en su dado poliedro regular:")
+            print("1- 4 caras")
+            print("2- 6 caras")
+            print("3- 8 caras")
+            print("4- 12 caras")
+            print("5- 20 caras")
+            option = int(input())
+            if option < 1 or option > 5:
+                raise InterruptedError()
+            resultado.append(optionsMap[option])
+            print("N = Numero de veces que se lanza el dado: ")
+            resultado.append(int(input()))
+            print("K = Experimentos: ")
+            resultado.append(int(input()))
+            invalidOption = False
+        except:
+            print("\nRespuesta invalida, por favor ingrese solo numeros o una opcion del menu valida\n")
+            resultado.clear()
     return resultado
 
 #main driver
